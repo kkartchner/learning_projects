34571e674e57de68f4e32b9aa53cc9f96cbf663b
Ky Kartchner <kartchnerky@gmail.com>
2020-03-06 19:47:35 -0700

    completed the todo list REACT crash course

---
+++ REACT/todo_list/src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment> 
+           <h1>About</h1> 
+           <p>This is the TodoList app v1.0.0. It is part of a <a style={{textDecoration: 'underline'}} href="https://www.youtube.com/watch?v=sBws8MSXN7A">React crash course</a>.</p>
+        </React.Fragment>
+    )
+}
